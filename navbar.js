966eb315170984a4ca7cbefe5d4fcafc3e68e830
Mick van Gelderen <mickvangelderen@gmail.com>
2015-08-05 22:01:18 +0200

    Removed automatic focus from menu open listener

--- navbar.js
+++ navbar.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
 	var mainNav = $('#jh-nav-main');
-	var mainNavLinks = mainNav.find('.jh-nav-contents a');
 
 	$('#jh-nav-main-toggle').on('click', function() {
 		if (mainNav.hasClass('active')) {
 			mainNav.removeClass('active');
 		} else {
 			mainNav.addClass('active');
-			mainNavLinks.first().focus();
 		}
 	});
 
 	mainNav.find('.jh-nav-background').on('click', function() {
 		mainNav.removeClass('active');
 	});
-
 });
